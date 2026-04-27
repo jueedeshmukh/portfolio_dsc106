14e2c422e12b1ba60fce7c24b4d7ad7c5eba2133
jueedeshmukh <juee.hd@gmail.com>
2026-04-27 13:43:19 -0700

    pie chart starting

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 console.log('projects data:', projects);
@@ -14,3 +16,5 @@ renderProjects(projects, projectsContainer, 'h2');
 window.renderProjects = renderProjects;
 window.projectsData = projects;
 window.projectsContainer = projectsContainer;
+
+let arcGenerator = d3.arc().innerRadius(10).outerRadius(50);
