84e6f8042dff0ca8914e63209a3d4c965bf6a022
jueedeshmukh <juee.hd@gmail.com>
2026-05-01 14:22:02 -0700

    pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -17,4 +17,35 @@ window.renderProjects = renderProjects;
 window.projectsData = projects;
 window.projectsContainer = projectsContainer;
 
-let arcGenerator = d3.arc().innerRadius(10).outerRadius(50);
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+
+// step 1.4
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
